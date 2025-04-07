203c51c51aaf2903f9d73c59eee66e64b1a41931
william-reges <william.reges1986@gmail.com>
2025-04-07 20:40:04 -0300

    update

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
 output "vpc" {
   description = "VPC"
-  value = aws_vpc.vpc.id
+  value       = aws_vpc.vpc.id
 }
 
 output "private-subnets" {
   description = "private-subnets"
-  value = [for k, v in aws_subnet.private-subnet : v.id]
+  value       = [for k, v in aws_subnet.private-subnet : v.id]
 }
 
 output "public-subnets" {
   description = "public-subnets"
-  value = [for k, v in aws_subnet.public-subnet : v.id]
+  value       = [for k, v in aws_subnet.public-subnet : v.id]
 }
 
 output "security_group_id_web" {
